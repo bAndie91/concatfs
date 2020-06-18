4e52e98351142231b6a291514f75b584cce166e5
Andreas Hrubak <802732+bAndie91@users.noreply.github.com>
2020-06-19 01:35:04 +0200

    dedup docs

--- src/concatfs.c
+++ src/concatfs.c
@@ -3,32 +3,6 @@
 
   Copyright 2015 Peter Schlaile (peter at schlaile dot de)
   Copyright 2020 Andras Hrubak (andreas at uucp dot hu)
-
-
-  WHAT DOES CONCATFS DO
-  
-  Concatfs mounts a directory.
-  Files in this directory are concatenation description files.
-  Concatenation description consists of lines, each line consists of
-  byte offset, length, and filename parts separated by space.
-
-  Say there is a file 'vfile.dat' with content:
-
-  1024 512 /data/file1.dat
-  0 512 /data/file2.dat
-
-  Empty lines are ignored.
-  
-  It manifests a file on the same name 'vfile.dat', but its first 512
-  byte is got from file1.dat from offset 1024, the second 512 bytes from
-  file2.dat from the beginning. So vfile.dat is 1024 bytes long.
-  
-  Make sure your constituting files are really at least that big in size,
-  that the prescribed length with the starting offset fits into it.
-  Otherwise you'll get unexpected data out.
-  
-  Make sure referred files exist and readable. Relative paths are okay,
-  they are relative to the source directory.
 */
 
 #define FUSE_USE_VERSION 26
